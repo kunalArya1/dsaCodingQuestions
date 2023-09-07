9d0b83855a74297f47946d61e25f50518cb58bd0
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-09-07 09:07:48 +0530

    HashSet

--- src/Hashing/Hashset/hashset.java
+++ src/Hashing/Hashset/hashset.java
@@ -1,6 +1,7 @@
 package Hashing.Hashset;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class hashset {
     public static void main(String[] args) {
@@ -9,8 +10,28 @@ public class hashset {
         set.add(20);
         set.add(30);
         set.add(40);
+        // size of hashset
+        System.out.println(set.size());
 
+        // Check whether the given value is there or not
+        System.out.println(set.contains(20));
+
+
+        // Removing the value
         System.out.println(set.remove(30));
-        System.out.println(set);
+
+        // Check whether the hashset is empty or not
+        System.out.println(set.isEmpty());
+        // traversing through using forEach loop
+        for (Integer i : set){
+            System.out.println(i + " ");
+        }
+
+        // traversing through iterator
+        Iterator<Integer> i = set.iterator();;
+
+        while (i.hasNext()){
+            System.out.println(i.next() + " ");
+        }
     }
 }
